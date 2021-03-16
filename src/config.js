fda84d898849dd7cd85a274a3a14354bbc6586be
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-03-15 22:30:40 -0500

    commit before cloning to desktop

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://desolate-falls-35884.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'spaced-repetition-auth-token',
 }
 // http://localhost:8000/api
